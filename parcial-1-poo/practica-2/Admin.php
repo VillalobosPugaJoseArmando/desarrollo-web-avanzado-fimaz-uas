faa93f24c600aa09d202ba12e1a2fe4449ca4bca
Armando Villalobos <joarvipu2003@gmail.com>
2026-03-09 08:09:49 -0700

    agregar clase Admin que extiende Usuario

--- parcial-1-poo/practica-2/Admin.php
+++ parcial-1-poo/practica-2/Admin.php
@@ -1,2 +1,11 @@
 <?php
 
+require_once 'Usuario.php';
+
+class Admin extends Usuario {
+    
+    // Método que retorna el rol de este usuario
+    public function getRol(): string {
+        return "Administrador";
+    }
+}
