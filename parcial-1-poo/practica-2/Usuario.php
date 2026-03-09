a5598668c128c7dcaa89ec83edaefc87a0039aa7
Armando Villalobos <joarvipu2003@gmail.com>
2026-03-09 09:01:51 -0700

    Correccion de unos detalles en cuanto a uso

--- parcial-1-poo/practica-2/Usuario.php
+++ parcial-1-poo/practica-2/Usuario.php
@@ -1,22 +1,20 @@
 <?php
 
+namespace App;
+
 class Usuario {
-    // Atributos (los "datos" del usuario)
     private string $nombre;
     private string $correo;
 
-    // Constructor: se ejecuta cuando creamos un Usuario
     public function __construct(string $nombre, string $correo) {
         $this->nombre = $nombre;
         $this->correo = $correo;
     }
 
-    // Método para obtener el nombre
     public function getNombre(): string {
         return $this->nombre;
     }
 
-    // Método para obtener el correo
     public function getCorreo(): string {
         return $this->correo;
     }
