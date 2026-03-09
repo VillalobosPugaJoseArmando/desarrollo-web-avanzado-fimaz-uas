f446ced38b81acd587445e6115ff96e16dd7d5a1
Armando Villalobos <joarvipu2003@gmail.com>
2026-03-09 09:03:06 -0700

    ultima correccion

--- parcial-1-poo/practica-2/index.php
+++ parcial-1-poo/practica-2/index.php
@@ -1,10 +1,17 @@
 <?php
 
-require_once 'Admin.php';
+spl_autoload_register(function (string $className) {
+    $archivo = __DIR__ . '/' . str_replace(['App\\', '\\'], ['', '/'], $className) . '.php';
+
+    if (file_exists($archivo)) {
+        require_once $archivo;
+    }
+});
+
+use App\Admin;
 
 $admin = new Admin("Armando Villalobos", "joarvipu2003@gmail.com");
 
 echo "Nombre: " . $admin->getNombre() . "<br>";
 echo "Correo: " . $admin->getCorreo() . "<br>";
-echo "Rol: " . $admin->getRol() . "<br>";
-
+echo "Rol: "    . $admin->getRol()    . "<br>";
