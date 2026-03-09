382896501d7bafe7fd900f20caba1fec67ce4d4c
Armando Villalobos <joarvipu2003@gmail.com>
2026-03-09 08:10:38 -0700

    agregar index.php con prueba de herencia

--- parcial-1-poo/practica-2/index.php
+++ parcial-1-poo/practica-2/index.php
@@ -1,3 +1,10 @@
 <?php
 
+require_once 'Admin.php';
+
+$admin = new Admin("Armando Villalobos", "joarvipu2003@gmail.com");
+
+echo "Nombre: " . $admin->getNombre() . "<br>";
+echo "Correo: " . $admin->getCorreo() . "<br>";
+echo "Rol: " . $admin->getRol() . "<br>";
 
