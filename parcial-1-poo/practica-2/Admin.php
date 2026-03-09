4f7b34c9df012ca326ecb90d321890c1c9c649bc
Armando Villalobos <joarvipu2003@gmail.com>
2026-03-09 09:02:35 -0700

    Correccion relacionada con el anterior commit

--- parcial-1-poo/practica-2/Admin.php
+++ parcial-1-poo/practica-2/Admin.php
@@ -1,10 +1,9 @@
 <?php
 
-require_once 'Usuario.php';
+namespace App;
 
 class Admin extends Usuario {
-    
-    // Método que retorna el rol de este usuario
+
     public function getRol(): string {
         return "Administrador";
     }
